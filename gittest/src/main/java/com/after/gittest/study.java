54774f137cf6e7e97a52a61f4dbb0152bb572b77
dengyi <16760108@qq.com>
2022-07-21 00:46:52 +0800

    第一次提交

---
+++ gittest/src/main/java/com/after/gittest/study.java
@@ -0,0 +1,9 @@
+package com.after.gittest;
+
+/**
+ * @author ck
+ */
+public class study {
+    int age;
+    String name;
+}
---
+++ gittest/src/test/java/com/after/gittest/GittestApplicationTests.java
@@ -0,0 +1,11 @@
+package com.after.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
